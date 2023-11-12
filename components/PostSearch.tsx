fb61e4d744e863671bb517d7a4e2801dbaf88b6a
Dmytro <DSerSmart@gmail.com>
2023-11-12 20:37:13 +0200

    PostSearch  swr

--- components/PostSearch.tsx
+++ components/PostSearch.tsx
@@ -1,16 +1,20 @@
 "use client";
-
+import useSWR from "swr";
+// import { usePosts } from "@/store";
 import { FormEventHandler, useState } from "react";
-import { usePosts } from "@/store";
+import { getPostsBySearch } from "@/services/getPosts";
 
 const PostSearch = () => {
+  const { mutate } = useSWR("posts");
   const [search, setSearch] = useState("");
-  const getPostsBySearch = usePosts((state) => state.getPostsBySearch);
+  // const getPostsBySearch = usePosts((state) => state.getPostsBySearch);
 
   const handleSubmit: FormEventHandler<HTMLFormElement> = async (event) => {
     event.preventDefault();
 
-    await getPostsBySearch(search);
+    const posts = await getPostsBySearch(search);
+
+    mutate(posts);
   };
 
   return (
