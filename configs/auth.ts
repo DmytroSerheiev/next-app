5e2253e36ac770c293832ead0b902f1d004e2334
Dmytro <DSerSmart@gmail.com>
2023-11-18 21:01:07 +0200

    fix error deploy

--- configs/auth.ts
+++ configs/auth.ts
@@ -1,11 +1,11 @@
 import type { AuthOptions, User } from "next-auth";
-import GoggleProvider from "next-auth/providers/google";
+import GoogleProvider from "next-auth/providers/google";
 import Credentials from "next-auth/providers/credentials";
 import { users } from "@/data/users";
 
 export const authConfig: AuthOptions = {
   providers: [
-    GoggleProvider({
+    GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_SECRET!,
     }),
