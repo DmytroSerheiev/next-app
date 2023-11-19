8f282c7e74517a25463c5ba6db4ba1bd0196d79a
Dmytro <DSerSmart@gmail.com>
2023-11-19 18:36:37 +0200

    secret: process.env.NEXTAUTH_SECRET,

--- configs/auth.ts
+++ configs/auth.ts
@@ -31,7 +31,7 @@ export const authConfig: AuthOptions = {
       },
     }),
   ],
-  // secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
   // pages: {
   //   signIn: "/signin",
   // },
